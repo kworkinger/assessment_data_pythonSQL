52f021ccc39e0cb106686e134bdb4179e95d7e9d
Kristian Workinger <kristianworkinger@gmail.com>
2021-11-12 12:32:00 -0700

    final commit

--- travel-journal/server/controller.js
+++ travel-journal/server/controller.js
@@ -1,4 +1,15 @@
+require("dotenv").config()
+const {CONNECTION_STRING} = process.env
+const Sequelize = require("sequelize")
 
+const sequelize = new Sequelize(CONNECTION_STRING, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+})
 
 module.exports = {
     seed: (req, res) => {
@@ -11,7 +22,13 @@ module.exports = {
                 name varchar
             );
 
-            *****YOUR CODE HERE*****
+            CREATE TABLE cities (
+                city_id SERIAL PRIMARY KEY,
+                name VARCHAR,
+                rating INTEGER,
+                country_id INTEGER REFERENCES countries(country_id)
+            );
+
 
             insert into countries (name)
             values ('Afghanistan'),
@@ -213,5 +230,17 @@ module.exports = {
             console.log('DB seeded!')
             res.sendStatus(200)
         }).catch(err => console.log('error seeding DB', err))
+    },
+    getCountries: (req, res) => {
+        sequelize.query(`
+            SELECT * FROM countries`
+        ).then(dbRes => res.status(200).send(dbRes[0]))
+        .catch(err => console.log('error sending db', err))
+    },
+    createCity: (req, res) => {
+        const {name, rating, countryId} = req.body
+        sequelize.query(`
+        INSERT INTO cities(name, rating, countryId)
+        VALUES ('${name}','${rating}', '${countryId}')`)
     }
 }
